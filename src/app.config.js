e8ad4ca2ee6a963130a2ac91fec3170bc34f7f2c
wangsong <415613212@qq.com>
2021-03-08 17:44:51 +0800

    backup

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,18 @@
 export default {
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-}
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+  useExtendedLib: { weui: true },
+  usingComponents: {
+    "mp-navigation-bar":
+      "/miniprogram_npm/weui-miniprogram/navigation-bar/navigation-bar",
+    "mp-cells": "/miniprogram_npm/weui-miniprogram/cells/cells",
+    "mp-cell": "/miniprogram_npm/weui-miniprogram/cell/cell",
+    "mp-icon": "/miniprogram_npm/weui-miniprogram/icon/icon",
+    "mp-dialog": "/miniprogram_npm/weui-miniprogram/dialog/dialog",
+  },
+};
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,48 +1,78 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import { add, minus, asyncAdd } from "../../actions/counter";
 
-import './index.scss'
+import "./index.scss";
 
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+@connect(
+  ({ counter }) => ({
+    counter,
+  }),
+  (dispatch) => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    },
+  })
+)
 class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  state = { open: false };
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
+    const { open } = this.state;
     return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+      <View className="index">
+        <View
+          onClick={() => {
+            this.setState({ open: true });
+          }}
+        >
+          打开dialog
+        </View>
+        <mp-dialog
+          show={open}
+          // onClick={handleSelect}
+          onButtonTap={(i) => {
+            console.log("onButtonTap", i);
+            this.setState({ open: !open });
+          }}
+          onClose={() => {
+            console.log("onClose");
+          }}
+          buttons={[
+            { text: "取消", extClass: "btn2" },
+            { text: "确定", extClass: "btn" },
+          ]}
+        >
+          <View
+            onClick={() => {
+              console.log("setOpen(!open)", open);
+              setOpen(!open);
+            }}
+            className="font16 title"
+          >
+            点击关闭
+          </View>
+        </mp-dialog>
       </View>
-    )
+    );
   }
 }
 
-export default Index
-
+export default Index;
